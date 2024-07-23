cd77dc33cd4e1fe04b9d7aca17a9e290490f0a03
cnWangFei <cnwangfei@github.com>
2024-07-23 18:03:54 +0800

    v24.07.23.001 新增url编码解码

--- SqlServerClr/Properties/AssemblyInfo.cs
+++ SqlServerClr/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Data.Sql;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("23.12.09.*")]
+[assembly: AssemblyVersion("24.07.23.*")]
 
---
+++ SqlServerClr/UserDefinedFunctions/UrlCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    // Url编码
+    public static SqlString UrlEncode(SqlString input)
+    {
+        if (input == SqlString.Null || input.Value == null)
+        {
+            return new SqlString("");
+        }
+        string encoded = Uri.EscapeDataString(input.Value);
+        return new SqlString(encoded);
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction]
+    // Url解码
+    public static SqlString UrlDecode(SqlString input)
+    {
+        if (input == SqlString.Null || input.Value == null)
+        {
+            return new SqlString("");
+        }
+        string decoded = Uri.UnescapeDataString(input.Value);
+        return new SqlString(decoded);
+    }
+};
+
